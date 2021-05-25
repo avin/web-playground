60474dfed30bada68426b1b70d3ec53f85756714
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-25 18:01:30 +0000

    Deploy avin/web-playground to github.com/avin/web-playground.git:gh-pages

---
+++ views/exp-7-dialog-window/js/index.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.open-dialog').forEach((el) => {
+    el.addEventListener('click', () => {
+        document.querySelector('.dialog').classList.remove('dialog_hidden');
+    });
+});
+
+document.querySelectorAll('.close-dialog, .dialog__wrapper').forEach((el) => {
+    el.addEventListener('click', () => {
+        document.querySelector('.dialog').classList.add('dialog_hidden');
+    });
+});
+
+document.querySelectorAll('.dialog__window').forEach((el) => {
+    el.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
+});
