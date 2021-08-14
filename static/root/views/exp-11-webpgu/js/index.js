6d55c624f48629d4ce40efa14227422c92ea66ec
a.vinogradov <a.vinogradov@bpcbt.com>
2021-08-14 11:18:19 +0300

    WORKING_PASS_POST!!

--- static/root/views/exp-11-webpgu/js/index.js
+++ static/root/views/exp-11-webpgu/js/index.js
@@ -15,11 +15,25 @@
     const adapter = await navigator.gpu.requestAdapter();
     const device = await adapter.requestDevice();
 
-    const swapChainFormat = 'bgra8unorm';
+    const devicePixelRatio = window.devicePixelRatio || 1;
+    const presentationSize = [canvas.clientWidth * devicePixelRatio, canvas.clientHeight * devicePixelRatio];
+    const presentationFormat = context.getPreferredFormat(adapter);
 
-    const swapChain = context.configureSwapChain({
+    // const swapChainFormat = 'bgra8unorm';
+    //
+    // const swapChain = context.configureSwapChain({
+    //     device,
+    //     format: swapChainFormat,
+    // });
+
+    context.configure({
         device,
-        format: swapChainFormat,
+        format: presentationFormat,
+
+        // Specify we want both RENDER_ATTACHMENT and COPY_SRC since we
+        // will copy out of the swapchain texture.
+        usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
+        size: presentationSize,
     });
 
     const pipeline = device.createRenderPipeline({
@@ -36,7 +50,7 @@
             entryPoint: 'main',
             targets: [
                 {
-                    format: swapChainFormat,
+                    format: presentationFormat,
                 },
             ],
         },
@@ -44,25 +58,6 @@
             topology: 'triangle-list',
         },
     });
-
-    // Fetch the image and upload it into a GPUTexture.
-    let cubeTexture;
-    {
-        const img = document.createElement('img');
-        img.src = './img/Di-3d.png';
-        await img.decode();
-        const imageBitmap = await createImageBitmap(img);
-
-        cubeTexture = device.createTexture({
-            size: [imageBitmap.width, imageBitmap.height, 1],
-            format: 'rgba8unorm',
-            usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST | GPUTextureUsage.RENDER_ATTACHMENT,
-        });
-        device.queue.copyExternalImageToTexture({ source: imageBitmap }, { texture: cubeTexture }, [
-            imageBitmap.width,
-            imageBitmap.height,
-        ]);
-    }
 
     // -----------------------
 
@@ -73,6 +68,77 @@
 
     const bindGroup = device.createBindGroup({
         layout: pipeline.getBindGroupLayout(0),
+        entries: [
+            {
+                binding: 0,
+                resource: { buffer },
+            },
+            // {
+            //     binding: 1,
+            //     resource: device.createSampler({
+            //         magFilter: 'linear',
+            //         minFilter: 'linear',
+            //     }),
+            // },
+            // {
+            //     binding: 2,
+            //     resource: cubeTexture.createView(),
+            // },
+        ],
+    });
+
+    // -----------------------
+
+    const postPipeline = device.createRenderPipeline({
+        vertex: {
+            module: device.createShaderModule({
+                code: glslang.compileGLSL(window.SHADERS.vertex, 'vertex'),
+            }),
+            entryPoint: 'main',
+        },
+        fragment: {
+            module: device.createShaderModule({
+                code: glslang.compileGLSL(window.SHADERS.postFragment, 'fragment'),
+            }),
+            entryPoint: 'main',
+            targets: [
+                {
+                    format: presentationFormat,
+                },
+            ],
+        },
+        primitive: {
+            topology: 'triangle-list',
+        },
+    });
+
+    // // Fetch the image and upload it into a GPUTexture.
+    // let cubeTexture;
+    // {
+    //     const img = document.createElement('img');
+    //     img.src = './img/Di-3d.png';
+    //     await img.decode();
+    //     const imageBitmap = await createImageBitmap(img);
+    //
+    //     cubeTexture = device.createTexture({
+    //         size: presentationSize,
+    //         format: 'rgba8unorm',
+    //         usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST | GPUTextureUsage.RENDER_ATTACHMENT,
+    //     });
+    //     device.queue.copyExternalImageToTexture({ source: imageBitmap }, { texture: cubeTexture }, [
+    //         imageBitmap.width,
+    //         imageBitmap.height,
+    //     ]);
+    // }
+
+    const cubeTexture = device.createTexture({
+        size: presentationSize,
+        format: presentationFormat,
+        usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST,
+    });
+
+    const postBindGroup = device.createBindGroup({
+        layout: postPipeline.getBindGroupLayout(0),
         entries: [
             {
                 binding: 0,
@@ -94,6 +160,17 @@
 
     // -----------------------
 
+    const renderPassDescriptor = {
+        colorAttachments: [
+            {
+                view: undefined, // Assigned later
+
+                loadValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
+                storeOp: 'store',
+            },
+        ],
+    };
+
     const timeBufferData = new Float32Array([0]);
     const mouseBufferData = new Float32Array([0, 0]);
     const resolutionBufferData = new Float32Array([0, 0]);
@@ -102,24 +179,35 @@
         timeBufferData[0] = timestamp / 1000;
         device.queue.writeBuffer(buffer, 0, timeBufferData.buffer, 0, 4);
 
+        const swapChainTexture = context.getCurrentTexture();
+        renderPassDescriptor.colorAttachments[0].view = swapChainTexture.createView();
+
         const commandEncoder = device.createCommandEncoder();
-        const textureView = swapChain.getCurrentTexture().createView();
-
-        const renderPassDescriptor = {
-            colorAttachments: [
-                {
-                    view: textureView,
-                    loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-                    storeOp: 'store',
-                },
-            ],
-        };
-
         const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+
         passEncoder.setPipeline(pipeline);
         passEncoder.setBindGroup(0, bindGroup);
         passEncoder.draw(6, 1, 0, 0);
         passEncoder.endPass();
+
+        // Copy the rendering results from the swapchain into |cubeTexture|.
+        commandEncoder.copyTextureToTexture(
+            {
+                texture: swapChainTexture,
+            },
+            {
+                texture: cubeTexture,
+            },
+            presentationSize,
+        );
+
+        renderPassDescriptor.colorAttachments[0].view = swapChainTexture.createView();
+
+        const postPassEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+        postPassEncoder.setPipeline(postPipeline);
+        postPassEncoder.setBindGroup(0, postBindGroup);
+        postPassEncoder.draw(6, 1, 0, 0);
+        postPassEncoder.endPass();
 
         device.queue.submit([commandEncoder.finish()]);
         requestAnimationFrame(frame);
--- static/root/views/exp-11-webpgu/shaders/postFragment.glsl
+++ static/root/views/exp-11-webpgu/shaders/postFragment.glsl
@@ -6,10 +6,25 @@ layout(set = 0, binding = 0) uniform UBO {
   float resolution_x;
   float resolution_y;
 };
+layout(set = 0, binding = 1) uniform sampler baseSampler;
+layout(set = 0, binding = 2) uniform texture2D environmentMap;
+
 layout(location = 0) out vec4 outColor;
 
 // --------- END-SHADER-TOY-CODE-HERE ------------
 
 void main() {
-    outColor = vec4(1.,0.,0.,1.);
+
+  vec2 fragCoord = gl_FragCoord.xy;
+
+  vec2 iResolution = vec2(resolution_x, resolution_y);
+
+  vec2 uv =
+      (fragCoord - iResolution.xy * 0.5) / min(iResolution.y, iResolution.x);
+
+  vec4 f_color = texture(sampler2D(environmentMap, baseSampler), uv);
+
+  f_color.xy += uv.xy;
+
+  outColor = f_color;
 }
