43feb0709eee386c2ea40ed5d024cfa0990ed285
a.vinogradov <a.vinogradov@bpcbt.com>
2025-11-09 21:40:33 +0300

    upd

--- static/root/gym/4/4.11/script.ts
+++ static/root/gym/4/4.11/script.ts
@@ -5,8 +5,10 @@ class VirtualScroll {
   container: HTMLElement;
   items: any[];
   itemHeight: number;
-  renderItem: (item: any) => string;
-  itemsBlock: HTMLElement;
+  renderItem: (item: any) => HTMLElement;
+  inner: HTMLElement;
+
+  innerChildNodes = new Map<number, HTMLElement>();
 
   constructor(
     container: HTMLElement,
@@ -17,18 +19,24 @@ class VirtualScroll {
     this.container = container;
     this.items = items;
     this.itemHeight = itemHeight;
-    this.renderItem = renderItem;
+    this.renderItem = (item) => {
+      const itemHtml = renderItem(item);
+      const template = document.createElement('template');
+      template.innerHTML = itemHtml.trim();
+      return template.content.firstElementChild! as HTMLElement;
+    };
 
-    const itemsBlock = document.createElement('div');
-    this.itemsBlock = itemsBlock;
-    itemsBlock.style.position = 'absolute';
-    itemsBlock.style.width = '100%';
-    itemsBlock.style.willChange = 'transform';
+    // const itemsBlock = document.createElement('div');
+    // this.itemsBlock = itemsBlock;
+    // itemsBlock.style.position = 'absolute';
+    // itemsBlock.style.width = '100%';
+    // itemsBlock.style.willChange = 'transform';
 
     const inner = document.createElement('div');
+    this.inner = inner;
     inner.style.height = `${this.items.length * this.itemHeight}px`;
     inner.style.position = 'relative';
-    inner.append(itemsBlock);
+    // inner.append(itemsBlock);
 
     this.container.append(inner);
 
@@ -36,9 +44,14 @@ class VirtualScroll {
     this.render();
   }
 
-  // Рендерить только видимые элементы + buffer
-  // При скролле обновлять отображаемые элементы
-  // Создать "виртуальную" высоту контейнера
+  createItemEl(i) {
+    const el = this.renderItem(this.items[i]);
+    el.style.position = 'absolute';
+    el.style.width = '100%';
+    el.style.transform = `translateY(${i * this.itemHeight}px)`;
+
+    return el;
+  }
 
   render(): void {
     const BUFFER = 5;
@@ -50,15 +63,27 @@ class VirtualScroll {
     startItems = Math.max(0, startItems - BUFFER);
     endItems = Math.min(endItems + BUFFER, this.items.length);
 
-    this.itemsBlock.style.transform = `translateY(${
-      startItems * this.itemHeight
-    }px)`;
+    // Удаляем элементы вне диапазона
+    this.innerChildNodes.forEach((child, idx) => {
+      if (idx < startItems || idx >= endItems) {
+        child.remove();
+        this.innerChildNodes.delete(idx);
+      }
+    });
 
-    const htmlItems: string[] = [];
+    // Добавляем новые элементы
+    const fragment = document.createDocumentFragment();
     for (let i = startItems; i < endItems; i++) {
-      htmlItems.push(this.renderItem(this.items[i]));
+      if (!this.innerChildNodes.has(i)) {
+        const el = this.createItemEl(i);
+        this.innerChildNodes.set(i, el);
+        fragment.appendChild(el);
+      }
     }
-    this.itemsBlock.innerHTML = htmlItems.join('');
+
+    if (fragment.hasChildNodes()) {
+      this.inner.appendChild(fragment);
+    }
   }
 }
 
