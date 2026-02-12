a6620f5ac9391ae0074ffa758375a5822a6775eb
a.vinogradov <a.vinogradov@bpcbt.com>
2026-02-12 20:31:07 +0300

    upd

--- pages/0021-card-form/src/App.tsx
+++ pages/0021-card-form/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   const [layoutMode, setLayoutMode] = createSignal<LayoutMode>('normal');
 
   return (
-    <div class="mt-[50px] flex flex-col items-center gap-5">
+    <div class="mt-[50px] flex w-full flex-col items-center gap-5 px-[10px]">
       <CardFormModeSwitch value={layoutMode()} onChange={setLayoutMode} />
       <CardForm layoutMode={layoutMode()} />
     </div>
